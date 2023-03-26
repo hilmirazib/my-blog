7fbfda0ca867a059f146548e6c816a8ede99b649
hilmi razib yusuf <hilmi.inolabs@gmail.com>
2023-03-26 12:42:27 +0700

    dashboard admin panel setup, logout option

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,8 +17,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// admin all route
+Route::controller(AdminController::class)->group(function() {
+    Route::get('/admin/logout', 'destroy')->name('admin.logout');
+});
+
+
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__ . '/auth.php';
